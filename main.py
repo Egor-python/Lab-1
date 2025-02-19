a9107d5f1c9434b1b1b3c4a6bb6eea02e7d1fbb5
Egor <bonkegor@gmail.com>
2025-02-19 16:11:35 +0300

    Last Task

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-def is_correct(word: str) -> bool:
-    correct_word: str = 'hello'
-    correct_word_len = len(correct_word)
-    i = 0
-    l_count = 0
-    for letter in word:
-        if i == correct_word_len:
-            break
-        if l_count > 1 and letter == 'l':
-            continue
-        if letter == correct_word[i-1] and correct_word[i-1] != 'l':
-            continue
-        if letter == correct_word[i]:
-            if letter == 'l':
-                l_count += 1
-            i += 1
+from typing import Union
 
-    if i == correct_word_len:
-        return True
+def is_caps(word: str) -> Union[str, bool]:
+    # Если слово из 1 буквы
+    def one_letter_check() -> bool:
+        return len(word) == 1
+
+    def first_letter_check() -> bool:
+        return word[0] == word[0].lower()
+
+    def other_letters_check() -> bool:
+        return word[1:] == word[1:].upper()
+
+    def all_word_check() -> bool:
+        return word == word.upper()
+
+    if one_letter_check():
+        return "ONE"
+    elif all_word_check():
+        return 'ALL'
+    elif all([first_letter_check(), other_letters_check()]):
+        return 'PART'
+
+    return False
+
+
+def corrector(word: str, word_type: str) -> str:
+    if word_type == 'ONE':
+        if word == word.lower():
+            return word.upper()
+        else:
+            return word.lower()
+    elif word_type == 'ALL':
+        return word.lower()
+    elif word_type == 'PART':
+        return word[0].upper() + word[1:].lower()
+    return 'Something wrong'
+
+
+def main(word):
+    word_type = is_caps(word)
+    if not word_type:
+        print(word)
     else:
-        return False
+        print(corrector(word, word_type))
 
-
-word = input()
-answer: bool = is_correct(word)
-if answer:
-    print("YES")
-else:
-    print("NO")
+main(input())
